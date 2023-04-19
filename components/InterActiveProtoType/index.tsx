0f1e821573cc693c6d288f5a274c201370d166df
suganya05 <go2suganya@gmail.com>
2023-04-19 15:45:33 +0530

    chore: pull

---
+++ components/InterActiveProtoType/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./InterActiveProtoType.module.scss";
+
+const InterActiveProtoType: React.FC = () => {
+  return (
+    <div className={styles.interActiveProtoType}>
+      <div className={styles.heading}>
+        <h1>
+          We bring our client’s ideas to life and create an Interactive
+          prototype.
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default InterActiveProtoType;
